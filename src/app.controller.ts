4302322b0830eff4a8d1c3cfddd79ec6e6353d95
fredrickUdom <fredrickudom@gmail.com>
2024-10-03 23:54:21 +0100

    updating project configuration settup

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ResponseMessage } from './common/decorator/response.decorator';
+import { DATA_FETCH } from './common/constant/user.constant';
+import { Public } from './common/decorator/public.decorator';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @Public()
+  @ResponseMessage(DATA_FETCH)
   getHello(): string {
     return this.appService.getHello();
   }
---
+++ src/common/constant/user.constant.ts
@@ -0,0 +1,13 @@
+export const USER_UPDATED = 'User information updated successfully';
+export const ADMIN_SEEDER = 'User Seeded Successfully';
+export const LOGGED_IN = 'User successfully logged in';
+export const LOGGED_OUT = 'Successfully logged out';
+export const FORGOT_PWD_RESET = 'Password Created. Login with new password.';
+export const PASSWORD_UPDATED =
+  'Password updated successfully and should be used on next login.';
+export const DATA_FETCH = 'Data fetched successfully';
+export const UPDATE_IMAGE = 'Picture changed successfully';
+export const REMOVE_IMAGE = 'Picture removed successfully';
+export const DELETE_USER = 'User deleted successfully';
+export const ACCOUNT_ACTIVATED= 'Account activated successfully'
+export const ACCOUNT_NOTACTIVATED= 'Account failed to activate'
